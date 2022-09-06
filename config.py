719899fa4c0efea4f3715b5fc729b932178c272c
Sergey Ugryumov <56644580+Reventaine@users.noreply.github.com>
2022-09-06 15:06:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
-githubToken = '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'
+githubToken = 'GITHUBTOKEN'
 
-geniusToken = 'y68cnKKehPUYg71FEJecoZHFF5Wniyyuri0-Ea7H3yDhV6wKAGsTODyhn2yElA-a'
+geniusToken = 'GENIUSTOKEN'
 
 # spotify auth:
-client_id = "d57ce86eacaf4f4282d1da003edc2275"
-client_secret = "1e8652aa32dc4c4a94bab6613bf8f085"
-redirect_uri = "http://127.0.0.1:9090"
-url = 'https://accounts.spotify.com/authorize?client_id=d57ce86eacaf4f4282d1da003edc2275&response_type=code&redirect_uri=http%3A%2F%2F127.0.0.1%3A9090&scope=user-read-currently-playing'
-
-telegramToken = "5453334258:AAGTg24fHyY_WpuB6Cc8uo2hWBtjJN-nhzM"
+client_id = "CLIENTID"
+client_secret = "CLIENTSECRET"
+redirect_uri = "URI"
+url = 'SPOTIFY_AUTH_LINK'
+telegramToken = "TELEGRAMTOKEN"
