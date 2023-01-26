4d2c88783cc4210d920bc7c780dafd08d3eab25a
Sergey Ugryumov <56644580+Reventaine@users.noreply.github.com>
2023-01-26 13:56:11 +0300

    Update openai-bot.py

--- openai-bot.py
+++ openai-bot.py
@@ -112,7 +112,7 @@ async def get_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 if __name__ == '__main__':
-    application = Application.builder().token("5560967942:AAGgMQFSrXJBOwZCvV01g_3uhad2pjGlMoI").build()
+    application = Application.builder().token("TOKEN").build()
 
     conv_handler = ConversationHandler(
         entry_points=[
