69b0aa8688ba7cb4e59421afa9f7b33aa29555e1
Sergey Ugryumov <56644580+Reventaine@users.noreply.github.com>
2023-01-28 18:16:31 +0400

    Add files via upload

--- openai-bot.py
+++ openai-bot.py
@@ -1,8 +1,9 @@
 import openai
+from PIL import Image
 import logging
 import time
 from config import openaiToken as openaiToken
-from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram import Update
 
 from telegram.ext import (
     Application,
@@ -12,8 +13,6 @@ from telegram.ext import (
     MessageHandler,
     filters,
 )
-
-from telegram.error import TimedOut
 
 from telegram import __version__ as TG_VER
 
@@ -40,7 +39,8 @@ openai.api_key = openaiToken
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("/text to use text neuralink or /image to generate images")
+    await update.message.reply_text("/text to use text neuralink or /image to generate images \nUpload square photo "
+                                    "with white area you want to alternate")
     return ConversationHandler.END
 
 
@@ -52,22 +52,26 @@ async def text(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     prompt = update.message.text
 
-    msg = await update.message.reply_text(f"Building an answer...")
-    msg
+    try:
+        msg = await update.message.reply_text(f"Building an answer...")
+        msg
 
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=prompt,
-        temperature=0,
-        max_tokens=2048,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-    )
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=0.7,
+            max_tokens=3700,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+        )
 
-    await msg.edit_text(text=response['choices'][0]['text'])
-    time.sleep(1)
-    return await start(update, context)
+        await msg.edit_text(text=response['choices'][0]['text'])
+        time.sleep(1)
+        return await start(update, context)
+
+    except:
+        await update.message.reply_text(text="Error, please try again")
 
 
 async def image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -81,6 +85,45 @@ async def get_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f"Building an images...")
 
     response = openai.Image.create(
+        prompt=prompt,
+        n=5,
+        size="1024x1024"
+    )
+
+    for i in range(5):
+        await update.message.reply_photo(photo=response['data'][i]['url'])
+        time.sleep(1)
+    return await start(update, context)
+
+
+def convert_image():
+    img = Image.open("file.jpg")
+    img = img.convert("RGBA")
+
+    datas = img.getdata()
+
+    newData = []
+    for item in datas:
+        if item[0] == 255 and item[1] == 255 and item[2] == 255:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append(item)
+
+    img.putdata(newData)
+    img.resize((1024, 1024), Image.Resampling.LANCZOS).convert('RGBA').save("file.png", format="png")
+
+
+async def change_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    prompt = update.message.caption
+
+    await update.message.reply_text(f"Alternating image...")
+    file = await context.bot.get_file(update.message.photo[-1]['file_id'])
+    await file.download('file.jpg')
+
+    convert_image()
+
+    response = openai.Image.create_edit(
+        image=open('file.png', "rb"),
         prompt=prompt,
         n=3,
         size="1024x1024"
@@ -99,13 +142,14 @@ if __name__ == '__main__':
         entry_points=[
             CommandHandler("text", text),
             CommandHandler('image', image),
+            MessageHandler(filters.PHOTO, change_image)
         ],
         states={
             CHAT: [
                 MessageHandler(filters.TEXT & (~ filters.COMMAND) & (~ filters.Regex('Image')), chat)
             ],
             IMAGE: [
-                MessageHandler(filters.Regex("^(Image)$"), image),
+                MessageHandler(filters.Regex("Image"), image),
                 MessageHandler(filters.TEXT & (~ filters.COMMAND) & (~ filters.Regex('Image')), get_image)
             ],
         },
