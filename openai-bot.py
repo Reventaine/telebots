c1ec2df321c70d579d0f3496516ed12f95e9b315
Sergey Ugryumov <56644580+Reventaine@users.noreply.github.com>
2023-01-30 14:17:55 +0400

    Add files via upload

--- openai-bot.py
+++ openai-bot.py
@@ -39,8 +39,18 @@ openai.api_key = openaiToken
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("/text to use text neuralink or /image to generate images \nUpload square photo "
-                                    "with white area you want to alternate")
+    await update.message.reply_text("/text to use text neuralink or /image to generate images")
+    return ConversationHandler.END
+
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("/text to use text neuralink Chat-GPT3, which can complete text or provide a "
+                                    "dialog, works in different languages \n/image to generate images from "
+                                    "text\nUpload a photo without any commands to alternate this photo."
+                                    "\n The photo must be square with the white area that you want to alter (you can "
+                                    "change image via telegram, crop it to a square and paint it"
+                                    "in white). Add a caption to specify how you want to alter it."
+                                    "\nWithout a caption provided there will be a full alternate image by neuralink")
     return ConversationHandler.END
 
 
@@ -75,14 +85,14 @@ async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Enter a request for image or /start")
+    await update.message.reply_text("Enter a request for an image or /text")
     return IMAGE
 
 
 async def get_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     prompt = update.message.text
 
-    await update.message.reply_text(f"Building an images...")
+    await update.message.reply_text(f"Building images...")
 
     response = openai.Image.create(
         prompt=prompt,
@@ -116,27 +126,39 @@ def convert_image():
 async def change_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     prompt = update.message.caption
 
-    await update.message.reply_text(f"Alternating image...")
+    await update.message.reply_text(f"Alternating images...")
     file = await context.bot.get_file(update.message.photo[-1]['file_id'])
     await file.download('file.jpg')
 
     convert_image()
 
-    response = openai.Image.create_edit(
-        image=open('file.png', "rb"),
-        prompt=prompt,
-        n=3,
-        size="1024x1024"
-    )
+    try:
+        response = openai.Image.create_edit(
+            image=open('file.png', "rb"),
+            prompt=prompt,
+            n=3,
+            size="1024x1024"
+        )
 
-    for i in range(3):
-        await update.message.reply_photo(photo=response['data'][i]['url'])
-        time.sleep(1)
+        for i in range(3):
+            await update.message.reply_photo(photo=response['data'][i]['url'])
+    except:
+        # image variation as a bonus:
+        response = openai.Image.create_variation(
+            image=open("file.png", "rb"),
+            n=1,
+            size="1024x1024"
+        )
+        image_url = response['data'][0]['url']
+
+        await update.message.reply_photo(photo=image_url, caption='Bonus')
+
+    time.sleep(1)
     return await start(update, context)
 
 
 if __name__ == '__main__':
-    application = Application.builder().token("TOKEN").build()
+    application = Application.builder().token("5560967942:AAGgMQFSrXJBOwZCvV01g_3uhad2pjGlMoI").build()
 
     conv_handler = ConversationHandler(
         entry_points=[
@@ -157,5 +179,6 @@ if __name__ == '__main__':
     )
 
     application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('help', help))
     application.add_handler(conv_handler)
     application.run_polling()
